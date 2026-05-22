2f7ee31b5410bd6ab24f9ce23ed51bdea0d3f6bf
tloaiza <t.loaiza@uniandes.edu.co>
2026-05-22 10:26:34 -0500

    cambios

--- Taller 5 Hamburguesas/tests/uniandes/dpoo/hamburguesas/tests/ProductoMenuTest.java
+++ Taller 5 Hamburguesas/tests/uniandes/dpoo/hamburguesas/tests/ProductoMenuTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uniandes.dpoo.hamburguesas.mundo.ProductoMenu;
+
 
 public class ProductoMenuTest {
-
+      
+	//el atributo que usamos en todos los casos
+	private ProductoMenu producto;
 }
